99f529e5d6f992640b0a5c9d50eb3fd3cd4147bd
Fellipe <araujo.fellipe@hotmail.com>
2021-06-23 12:38:34 -0300

    Create New Room page.

---
+++ src/pages/NewRoom.tsx
@@ -0,0 +1,35 @@
+import illustrationImg from "../assets/illustration.svg";
+import logoImg from "../assets/logo.svg";
+import googleIconImg from "../assets/google-icon.svg";
+
+import "../styles/auth.scss";
+import { Button } from "../components/Button";
+
+export function NewRoom() {
+  return (
+    <div id="page-auth">
+      <aside>
+        <img
+          src={illustrationImg}
+          alt="Ilustração simbolizando perguntas e respostas"
+        />
+        <strong>Crie salas de Q&amp;A ao-vivo</strong>
+        <p>Tire as dúvidas de sua audiência em tempo-real</p>
+      </aside>
+
+      <main>
+        <div className="main-content">
+          <img src={logoImg} alt="Letmeask" />
+          <h2>Criar uma nova sala</h2>
+          <form>
+            <input type="text" placeholder="Digite o código da sala" />
+            <Button type="submit">Entrar na sala</Button>
+          </form>
+          <p>
+            Quer entrar em uma sala existente? <a href="#">Clique aqui</a>
+          </p>
+        </div>
+      </main>
+    </div>
+  );
+}
